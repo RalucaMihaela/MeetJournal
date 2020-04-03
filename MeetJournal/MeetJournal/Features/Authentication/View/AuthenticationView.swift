0dd8c49cc7cf79f584e5253468bd735eeaa7105b
RalucaMihaela <ionescu.raluca@agilefreaks.com>
2020-04-03 22:08:12 +0300

    Final retouch for authentication with FaceID

--- MeetJournal/MeetJournal/Features/Authentication/View/AuthenticationView.swift
+++ MeetJournal/MeetJournal/Features/Authentication/View/AuthenticationView.swift
@@ -14,15 +14,51 @@ struct AuthenticationView: View {
     @State var shouldShowMenu = false
     
     var body: some View {
-            ZStack {
-                Color.red
+        ZStack {
+            Color.init(UIColor.accent1)
+                .edgesIgnoringSafeArea(.vertical)
+            
+            VStack {
+                Image("logo")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 350)
+                    .padding(.top, 10)
                 
-                if self.shouldShowMenu {
-                    MenuTabView()
-                   
-                }
+                Spacer()
+                
+                ZStack {
+                    Rectangle()
+                        .foregroundColor(.white)
+                        .frame(width:200, height:200)
+                        .cornerRadius(20)
+                        .onTapGesture {
+                            self.viewModel.detectFaceID()
+                    }
+                    
+                    VStack {
+                        Image(systemName: "faceid")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width:50, height:50)
+                            .foregroundColor(Color(UIColor.accent1))
+                        
+                        Spacer()
+                            .frame(height: 30)
+                        
+                        Text("Tap to Login with")
+                        
+                        Text("Face ID")
+                            .font(.custom("ClanOT-Medium", size: 17))
+                            .foregroundColor(Color(UIColor.accent1))
+                    }
+                }.padding(.bottom, 200)
             }
-        .onAppear{
+            
+            if self.shouldShowMenu {
+                MenuTabView()
+            }
+        }.onAppear{
             self.viewModel.detectFaceID()
         }
         .onReceive(self.viewModel.$state) { value in
